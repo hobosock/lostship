378f3da790729ec67936b6e0c593a573d2cf6243
hobosock <76829496+hobosock@users.noreply.github.com>
2024-05-05 17:40:59 -0400

    working on combat functions

--- src/gamerules/combat.rs
+++ src/gamerules/combat.rs
@@ -1,9 +1,26 @@
+use crate::app::App;
+
 use super::{
     pilot::{PilotStatus, Rank},
     roll,
     ship::{Scout, ShipDamage},
     threat::{Fighter, Threats},
 };
+
+pub enum Targets {
+    Superficial,
+    FifthScout,
+    FourthScout,
+    ThirdScout,
+    SecondScout,
+    LeadScout,
+    Hull,
+    Engines,
+    MiningLaser,
+    ScoutingBay,
+    SickBay,
+    Sensors,
+}
 
 /// tracks all the information for combat phase
 /// formation fields are predefined arrays because it is not possible to field more than 6 ships
@@ -39,6 +56,93 @@ pub fn scout_attack(scout: &Scout) -> u64 {
         1
     } else if attack_result == 6 && scout.pilot.status == PilotStatus::Normal {
         2
+    } else {
+        0
+    }
+}
+
+/// enemy attack success roll
+pub fn enemy_attack() -> bool {
+    if roll(6) > 3 {
+        true
+    } else {
+        false
+    }
+}
+
+/// logic for enemy targeting - handles 1st round, 2nd round and after
+pub fn enemy_targeting(app: &mut App) -> Targets {
+    let combat = app.combat.clone().unwrap();
+    let roll_result = if combat.rounds > 1 {
+        roll(6) + roll(6)
+    } else {
+        roll(6)
+    };
+    if roll_result == 1 {
+        Targets::Superficial
+    } else if roll_result == 2 {
+        Targets::FifthScout
+    } else if roll_result == 3 {
+        Targets::FourthScout
+    } else if roll_result == 4 {
+        Targets::ThirdScout
+    } else if roll_result == 5 {
+        Targets::SecondScout
+    } else if roll_result == 6 {
+        Targets::LeadScout
+    } else if roll_result == 7 {
+        Targets::Hull
+    } else if roll_result == 8 {
+        Targets::Engines
+    } else if roll_result == 9 {
+        Targets::MiningLaser
+    } else if roll_result == 10 {
+        Targets::ScoutingBay
+    } else if roll_result == 11 {
+        Targets::SickBay
+    } else if roll_result == 12 {
+        Targets::Sensors
+    } else {
+        Targets::Hull
+    }
+}
+
+/// logic for damaging scout on hit
+pub fn scout_damage(scout: &mut Scout) -> String {
+    let roll_result = roll(6);
+    if roll_result == 1 {
+        "Superficial damage.".to_string()
+    } else if roll_result == 2 {
+        scout.pilot.status = PilotStatus::Injured;
+        "Pilot injured.".to_string()
+    } else if roll_result == 3 {
+        scout.pilot.status = PilotStatus::Kia;
+        "Pilot KIA".to_string()
+    } else if roll_result == 4 {
+        scout.ship.damage = ShipDamage::Half;
+        "Scout at 50% damage".to_string()
+    } else if roll_result == 5 {
+        scout.ship.damage = ShipDamage::Inoperable;
+        "Scout Inoperable, recalling now...".to_string()
+    } else {
+        scout.pilot.status = PilotStatus::Kia;
+        scout.ship.damage = ShipDamage::Destroyed;
+        "Scout destroyed, pilot KIA".to_string()
+    }
+}
+
+/// logic for mining laser attack
+pub fn mining_laser(upgraded: bool) -> u64 {
+    let mut roll_result = roll(6);
+    if upgraded {
+        roll_result += 1;
+    }
+    if roll_result >= 4 && roll_result <= 5 {
+        1
+    } else if roll_result == 6 {
+        2
+    } else if roll_result == 7 {
+        3
     } else {
         0
     }
